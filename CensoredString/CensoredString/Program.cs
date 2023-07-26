fefc41bd5eff82a62342d4b911b0f327fbeb6337
Abdur Rehman <abdurrehmanmughal1@gmail.com>
2023-07-26 16:19:19 +0500

    feat: CensoredStringVowels

---
+++ CensoredString/CensoredString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Text;
+
+namespace CensoredString
+{
+    public class program
+    {
+       
+        public static string Uncensor(string str, string vowels)
+        {
+            string uncensored = "";
+            int vowelCounter = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                uncensored += (str[i] != '*' ? str[i] : vowels[vowelCounter++]);
+            }
+
+            return uncensored;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(Uncensor("Wh*r* d*d my v*w*ls g*?", "eeioeo"));
+            Console.WriteLine(Uncensor("abcd", ""));
+            Console.WriteLine(Uncensor("*PP*RC*S*", "UEAE"));
+            Console.WriteLine(Uncensor("sl*wm*t**n", "ooio"));
+
+        }
+    }
+}
